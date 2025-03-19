2b069e3f224aa7f8ca05b108db0f215a99678447
Paoxyz <paobutdiff@gmail.com>
2025-03-19 18:51:41 +0800

    Update Student's Record.cpp

--- Student's Record.cpp
+++ Student's Record.cpp
@@ -1,35 +1,36 @@
-// sir may error po yan pero pinass ko na lang, pag i-choose nyo po yung 4 nag i-infinite loop po sya
-// di ko na alam ang gagawin dyan sir, na-stress ako saglit
 #include <iostream>
 using namespace std;
 
+struct Student {
+    int id;
+    string firstName;
+    string lastName;
+    string course;
+    float gpa;
+};
+
 int main() {
-    int id[100];
-    string firstName[100], lastName[100], course[100];
-    float gpa[100];
-    int count = 0, choice;
+    Student students[100];
+    int count = 0;
+    int choice;
 
     while (true) {
         cout << "\nMenu:\n";
-        cout << "1. Add Student\n
-        cout <<"2. Edit Student\n" 
-        cout << "3. Delete Student\n"
-        cout << "4. View Students\n"
-        cout <<"5. Exit\n";
+        cout << "1. Add Student\n2. Edit Student\n3. Delete Student\n4. View Students\n5. Exit\n";
         cout << "Enter choice: ";
         cin >> choice;
-        
+
         if (choice == 1) {
             cout << "Enter ID: ";
-            cin >> id[count];
+            cin >> students[count].id;
             cout << "Enter First Name: ";
-            cin >> firstName[count];
+            cin >> students[count].firstName;
             cout << "Enter Last Name: ";
-            cin >> lastName[count];
+            cin >> students[count].lastName;
             cout << "Enter Course: ";
-            cin >> course[count];
+            cin >> students[count].course;
             cout << "Enter GPA: ";
-            cin >> gpa[count];
+            cin >> students[count].gpa;
             count++;
             cout << "Student added!\n";
         }
@@ -40,7 +41,7 @@ int main() {
             cin >> editID;
 
             for (int i = 0; i < count; i++) {
-                if (id[i] == editID) {
+                if (students[i].id == editID) {
                     found = i;
                     break;
                 }
@@ -48,13 +49,13 @@ int main() {
 
             if (found != -1) {
                 cout << "Enter New First Name: ";
-                cin >> firstName[found];
+                cin >> students[found].firstName;
                 cout << "Enter New Last Name: ";
-                cin >> lastName[found];
+                cin >> students[found].lastName;
                 cout << "Enter New Course: ";
-                cin >> course[found];
+                cin >> students[found].course;
                 cout << "Enter New GPA: ";
-                cin >> gpa[found];
+                cin >> students[found].gpa;
                 cout << "Student record updated!\n";
             } else {
                 cout << "Student not found!\n";
@@ -67,7 +68,7 @@ int main() {
             cin >> deleteID;
 
             for (int i = 0; i < count; i++) {
-                if (id[i] == deleteID) {
+                if (students[i].id == deleteID) {
                     found = i;
                     break;
                 }
@@ -75,39 +76,35 @@ int main() {
 
             if (found != -1) {
                 for (int i = found; i < count - 1; i++) {
-                    id[i] = id[i + 1];
-                    firstName[i] = firstName[i + 1];
-                    lastName[i] = lastName[i + 1];
-                    course[i] = course[i + 1];
-                    gpa[i] = gpa[i + 1];
+                    students[i] = students[i + 1];  
                 }
-            count--;
-           cout << "Student deleted!\n";
+                count--;
+                cout << "Student deleted!\n";
             } else {
                 cout << "Student not found!\n";
             }
         }
 
-     else if (choice == 4) {
-        if (count == 0) {
-          cout << "No students to display.\n";
-         } else {
-      cout << "\nStudent List:\n";
-      for (int i = 0; i < count; i++) {
-       cout << "ID: " << id[i] << ", Name: " << firstName[i] << " " << lastName[i]
-     << ", Course: " << course[i] << ", GPA: " << gpa[i] << endl;
-         }
-         }
+        else if (choice == 4) {
+            if (count == 0) {
+                cout << "No students to display.\n";
+            } else {
+                cout << "\nStudent List:\n";
+                for (int i = 0; i < count; i++) {
+                    cout << "ID: " << students[i].id << ", Name: " << students[i].firstName << " " << students[i].lastName
+                         << ", Course: " << students[i].course << ", GPA: " << students[i].gpa << endl;
+                }
+            }
         }
-            
-    else if (choice == 5) {
-   cout << "Exiting program...\n";
-    break;
-     }
 
-    else {
-    cout << "Invalid choice! Try again.\n";   
-    }
+        else if (choice == 5) {
+            cout << "Exiting program...\n";
+            break;
+        }
+
+        else {
+            cout << "Invalid choice! Try again.\n";
+        }
     }
 
     return 0;
